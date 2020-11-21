d86e0544f6570d4efdb46786d55aa87894f2b0f4
AhammadNaim <airnanos@gmail.com>
2020-11-21 09:39:10 +0600

    Add files via upload

---
+++ livesearch.php
@@ -0,0 +1,47 @@
+
+<?php
+$xmlDoc=new DOMDocument();
+
+
+//get the q parameter from URL
+$q=$_GET["q"];
+
+//lookup all links from the xml file if length of q>0
+if (strlen($q)>0) {
+	$xmlDoc->load("search/"+$q[0]);
+
+$x=$xmlDoc->getElementsByTagName('link');
+  $hint="";
+  for($i=0; $i<($x->length); $i++) {
+    $y=$x->item($i)->getElementsByTagName('title');
+    $z=$x->item($i)->getElementsByTagName('url');
+    if ($y->item(0)->nodeType==1) {
+      //find a link matching the search text
+      if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
+        if ($hint=="") {
+          $hint="<li role='presentation'> <a role='menuitem' href='" .
+          $z->item(0)->childNodes->item(0)->nodeValue .
+          "' target='_blank'>" .
+          $y->item(0)->childNodes->item(0)->nodeValue . "</a></li>";
+        } else {
+          $hint=$hint . "<li role='presentation'><a role='menuitem' href='" .
+          $z->item(0)->childNodes->item(0)->nodeValue .
+          "' target='_blank'>" .
+          $y->item(0)->childNodes->item(0)->nodeValue . "</a></li>";
+        }
+      }
+    }
+  }
+}
+
+// Set output to "no suggestion" if no hint was found
+// or to the correct values
+if ($hint=="") {
+  $response="no suggestion";
+} else {
+  $response=$hint;
+}
+
+//output the response
+echo $response;
+?> 
